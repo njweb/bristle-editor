aa6fec48cfc6e4c1369a65d3cf885cc84ef25dbf
nick jenkins <nickjenkinsweb@gmail.com>
2019-12-28 12:17:05 -0800

    cleanup

--- src/scripts/main.app.js
+++ src/scripts/main.app.js
@@ -1,17 +1,12 @@
 /* eslint-disable react/jsx-filename-extension */
 import { h, render } from 'preact';
-import RenderPlane from './RenderPlane';
 import EditorCanvas from './EditorCanvas';
-import AppComponent from './AppComponent';
 
 import buildEditorSystem from './systems/editorSystem';
 
 const editorSystem = buildEditorSystem();
-//const MyApp = () => <RenderPlane />;
 
 const doRender = state => {
-//  render(<AppComponent state={state} dispatch={() => {}} />,
-//    document.querySelector('.app-anchor'));
   render(<EditorCanvas editorSystem={editorSystem} />,
     document.querySelector('.app-anchor'));
 };
